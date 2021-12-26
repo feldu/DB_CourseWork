f490a00b6785b31f88cef88911cb23f020ef6b9d
Egor Ostryakov <eg.ostr@mail.ru>
2021-12-26 22:30:02 +0300

    frontend init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {Center, Text} from "@chakra-ui/react";
+import React from "react";
+
+function App() {
+    return (
+        <Center w="full" h="400px">
+            <Text>Is it working?</Text>
+        </Center>
+    );
+}
+
+export default App;
