e887ec63bfdc375e1d2682052a846d250ad30c31
Egor Ostryakov <eg.ostr@mail.ru>
2022-01-02 16:05:12 +0300

    add devtools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {ChakraProvider} from '@chakra-ui/react'
+import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import {applyMiddleware, createStore} from "redux";
 import {rootReducer} from "./redux/reducers/rootReducer";
 import {Provider} from "react-redux";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <ChakraProvider>
