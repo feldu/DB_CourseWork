c567122843b670174e585c6fb77aa9d4adaed3bf
Семён Шибаев <s.shibaev@kt.team>
2025-02-19 00:52:22 +0300

    рефакторинг димона

--- backend/src/main/java/db/coursework/dto/UserDTO.java
+++ backend/src/main/java/db/coursework/dto/UserDTO.java
@@ -1,9 +1,11 @@
 package db.coursework.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class UserDTO {
     private String username;
